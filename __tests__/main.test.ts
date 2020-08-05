f2ef0586014ef7ffc23d82925cf439d077a87fe8
Jörg Henne <j.henne@levigo.de>
2020-08-05 15:46:13 +0200

    Improved version handling

---
+++ __tests__/main.test.ts
@@ -0,0 +1,102 @@
+import * as process from 'process';
+import * as cp from 'child_process';
+import * as path from 'path';
+import * as core from "@actions/core";
+import { run } from "../src/lib";
+import * as fs from "fs";
+import intercept from 'intercept-stdout';
+
+jest.setTimeout(30000000);
+prepareMockRepo();
+process.env.INPUT_tag_prefix = "my-prefix-";
+process.env.INPUT_release_branches = "master";
+process.env.GITHUB_REF = "refs/heads/master";
+
+class StdoutCapture {
+    captured: string;
+    unhookIntercept: () => void;
+
+    constructor() {
+        this.captured = "";
+        this.unhookIntercept = intercept((data: string) => {
+            this.captured += data;
+            return data;
+        });
+    }
+
+    stopCapture() {
+        this.unhookIntercept();
+    }
+}
+
+test('builds major release', async () => {
+    process.env.GITHUB_SHA = "7f42496b3e66a58642227e2200e6405c07537e5d";
+    cp.execSync("git checkout 7f42496b3e66a58642227e2200e6405c07537e5d");
+
+    const cap = new StdoutCapture();
+
+    await run();
+
+    cap.stopCapture();
+
+    expect(cap.captured).toContain("set-output name=new_tag,::my-prefix-2.0.0");
+});
+
+test('builds minor release', async () => {
+    process.env.GITHUB_SHA = "5b6c18f88b275b9654ea6f0448126c743133b5ff";
+    cp.execSync("git checkout 5b6c18f88b275b9654ea6f0448126c743133b5ff");
+
+    const cap = new StdoutCapture();
+
+    await run();
+
+    cap.stopCapture();
+
+    expect(cap.captured).toContain("set-output name=new_tag,::my-prefix-1.1.0");
+});
+
+test('builds patch release', async () => {
+    process.env.GITHUB_SHA = "8fe0159ec6ed3bb669765fbce6ed42a0b4adede5";
+    cp.execSync("git checkout 8fe0159ec6ed3bb669765fbce6ed42a0b4adede5");
+
+    const cap = new StdoutCapture();
+
+    await run();
+
+    cap.stopCapture();
+
+    expect(cap.captured).toContain("set-output name=new_tag,::my-prefix-1.0.1");
+});
+
+
+function prepareMockRepo() {
+    if (!fs.existsSync("tmp")) {
+        fs.mkdirSync("tmp");
+    }
+
+    process.chdir("tmp");
+
+    if (!fs.existsSync("github-tag-action-mock-repo")) {
+        console.log("Cloning https://github.com/hennejg/github-tag-action-mock-repo.git");
+        cp.execSync("git clone https://github.com/hennejg/github-tag-action-mock-repo.git");
+    }
+
+    process.chdir("github-tag-action-mock-repo");
+}
+// test('wait 500 ms', async () => {
+//     const start = new Date();
+//     await wait(500);
+//     const end = new Date();
+//     var delta = Math.abs(end.getTime() - start.getTime());
+//     expect(delta).toBeGreaterThan(450);
+// });
+
+// // shows how the runner will run a javascript action with env / stdout protocol
+// test('test runs', () => {
+//     process.env['INPUT_MILLISECONDS'] = '500';
+//     const ip = path.join(__dirname, '..', 'lib', 'main.js');
+//     const options: cp.ExecSyncOptions = {
+//         env: process.env
+//     };
+//     console.log(cp.execSync(`node ${ip}`, options).toString());
+// });
---
+++ src/lib.ts
@@ -0,0 +1,211 @@
+import * as core from "@actions/core";
+import { exec as _exec } from "@actions/exec";
+import { context, GitHub } from "@actions/github";
+import semver, { ReleaseType } from "semver";
+import { analyzeCommits } from "@semantic-release/commit-analyzer";
+import { generateNotes } from "@semantic-release/release-notes-generator";
+
+const SEPARATOR = "==============================================";
+
+async function exec(command: string, args?: string[]) {
+  let stdout = "";
+  let stderr = "";
+
+  try {
+    const options = {
+      listeners: {
+        stdout: (data: Buffer) => {
+          stdout += data.toString();
+        },
+        stderr: (data: Buffer) => {
+          stderr += data.toString();
+        }
+      }
+    };
+
+    const code = await _exec(command, args, options);
+
+    return {
+      code,
+      stdout,
+      stderr
+    };
+  } catch (err) {
+    return {
+      code: 1,
+      stdout,
+      stderr,
+      error: err
+    };
+  }
+}
+
+export async function run() {
+  try {
+    const defaultBump = core.getInput("default_bump") as ReleaseType;
+    const tagPrefix = core.getInput("tag_prefix");
+    const releaseBranches = core.getInput("release_branches");
+    const createAnnotatedTag = core.getInput("create_annotated_tag");
+    const dryRun = core.getInput("dry_run");
+
+    const { GITHUB_REF, GITHUB_SHA } = process.env;
+
+    if (!GITHUB_REF) {
+      core.setFailed("Missing GITHUB_REF");
+      return;
+    }
+
+    if (!GITHUB_SHA) {
+      core.setFailed("Missing GITHUB_SHA");
+      return;
+    }
+
+    const preRelease = releaseBranches
+      .split(",")
+      .every(branch => !GITHUB_REF.replace("refs/heads/", "").match(branch));
+
+    await exec("git fetch --tags");
+
+    const hasTag = !!(await exec("git tag")).stdout.trim();
+    let tag = "";
+    let logs = "";
+
+    if (hasTag) {
+      console.log(await exec('pwd'));
+      const previousTagSha = (
+        await exec(`git rev-list --tags=${tagPrefix}* --topo-order --max-count=1`)
+      ).stdout.trim();
+      tag = (await exec(`git describe --tags ${previousTagSha}`)).stdout.trim();
+      logs = (
+        await exec(
+          `git log ${tag}..HEAD --pretty=format:'%s%n%b${SEPARATOR}' --abbrev-commit`
+        )
+      ).stdout.trim();
+
+      if (previousTagSha === GITHUB_SHA) {
+        core.debug("No new commits since previous tag. Skipping...");
+        core.setOutput("previous_tag", tag);
+        return;
+      }
+    } else {
+      tag = "0.0.0";
+      logs = (
+        await exec(
+          `git log --pretty=format:'%s%n%b${SEPARATOR}' --abbrev-commit`
+        )
+      ).stdout.trim();
+      core.setOutput("previous_tag", tag);
+    }
+
+    console.info(`Current tag is ${tag}`);
+
+    // for some reason the commits start with a `'` on the CI so we ignore it
+    const commits = logs
+      .split(SEPARATOR)
+      .map(x => ({ message: x.trim().replace(/(^['\s]+)|(['\s]+$)/g, "") }))
+      .filter(x => !!x.message);
+
+    core.debug(`Commits: ${commits}`);
+
+    var bump = await analyzeCommits(
+      {},
+      { commits, logger: { log: console.info.bind(console) } }
+    );
+    core.debug(`Bump type from commits: ${bump}`);
+
+    bump = bump || defaultBump;
+
+    core.debug(`Effective bump type: ${bump}`);
+
+    if (!bump) {
+      core.setFailed(`Nothing to bump - not building release`);
+      return;
+    }
+
+    const rawVersion = tag.replace(tagPrefix, '');
+    const incResult = semver.inc(rawVersion, bump || defaultBump);
+    core.debug(`SemVer.inc(${rawVersion}, ${bump || defaultBump}): ${incResult}`);
+    if (!incResult) {
+      core.setFailed(`SemVer inc rejected tag ${tag}`);
+      return;
+    }
+
+    const newVersion = `${incResult}${preRelease ? `-${GITHUB_SHA.slice(0, 7)}` : ""}`;
+    const newTag = `${tagPrefix}${newVersion}`;
+
+    core.setOutput("new_version", newVersion);
+    core.setOutput("new_tag", newTag);
+
+    core.debug(`New tag: ${newTag}`);
+
+    const changelog = await generateNotes(
+      {},
+      {
+        commits,
+        logger: { log: console.info.bind(console) },
+        options: {
+          repositoryUrl: `https://github.com/${process.env.GITHUB_REPOSITORY}`
+        },
+        lastRelease: { gitTag: tag },
+        nextRelease: { gitTag: newTag, version: newVersion }
+      }
+    );
+
+    core.setOutput("changelog", changelog);
+
+    if (preRelease) {
+      core.debug(
+        "This branch is not a release branch. Skipping the tag creation."
+      );
+      return;
+    }
+
+    const tagAlreadyExists = !!(
+      await exec(`git tag -l "${newTag}"`)
+    ).stdout.trim();
+
+    if (tagAlreadyExists) {
+      core.debug("This tag already exists. Skipping the tag creation.");
+      return;
+    }
+
+    core.info("dry_run: " + dryRun + " (" + typeof (dryRun) + ")");
+    if (dryRun) {
+      core.info("Dry run: not performing tag action.");
+      return;
+    }
+
+    const octokit = new GitHub(core.getInput("github_token"));
+
+    if (createAnnotatedTag === "true") {
+      core.debug(`Creating annotated tag`);
+
+      const tagCreateResponse = await octokit.git.createTag({
+        ...context.repo,
+        tag: newTag,
+        message: newTag,
+        object: GITHUB_SHA,
+        type: "commit"
+      });
+
+      core.debug(`Pushing annotated tag to the repo`);
+
+      await octokit.git.createRef({
+        ...context.repo,
+        ref: `refs/tags/${newTag}`,
+        sha: tagCreateResponse.data.sha
+      });
+      return;
+    }
+
+    core.debug(`Pushing new tag to the repo`);
+
+    await octokit.git.createRef({
+      ...context.repo,
+      ref: `refs/tags/${newTag}`,
+      sha: GITHUB_SHA
+    });
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
