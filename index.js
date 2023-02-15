f7ca4d9c528ac0d951af52358a52743214ed32d1
Liad Noam <liad.n@tevel-tech.com>
2023-02-15 18:21:47 +0200

    bump

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const fs = require('fs')
 const os = require('os')
 const { getTagSha, getTag, getLightTag, getCommits, checkTagExists, createTag, fetchTags } = require('./src/git');
 
+
 function setOutput(key, value) {
   const output = process.env['GITHUB_OUTPUT']
   // Temporary hack until core actions library catches up with github new recommendations
