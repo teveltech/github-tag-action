a023045986a350a73202512ee160639b9ca7b260
GitHub Action <action@github.com>
2023-02-15 17:03:32 +0000

    Pack with dependencies to dist/index.js

--- dist/index.js
+++ dist/index.js
@@ -23,14 +23,16 @@ const { getTagSha, getTag, getLightTag, getCommits, checkTagExists, createTag, f
 
 
 function setOutput(key, value) {
-  const output = process.env['GITHUB_OUTPUT']
-  console.log("🚀 ~ file: index.js:12 ~ setOutput ~ output", output)
+  // const output = process.env['GITHUB_OUTPUT']
+  // console.log("🚀 ~ file: index.js:12 ~ setOutput ~ output", output)
   // Temporary hack until core actions library catches up with github new recommendations
   // TODO: check for updates
 try {
-      fs.appendFileSync(output, `${key}=${value}${os.EOL}`)
+    core.setOutput(key,value)
+      // fs.appendFileSync(output, `${key}=${value}${os.EOL}`)
   
 } catch (error) {
+  console.log("🚀 ~ file: index.js:20 ~ setOutput ~ error", error)
   
 }}
 async function run() {
