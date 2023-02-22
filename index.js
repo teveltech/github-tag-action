d1b91d58200a4e855546009c99aeaf4a639de205
Liad Noam <52039539+Liad-n@users.noreply.github.com>
2023-02-22 18:51:04 +0200

    with the help of god (removing ## from value)

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const { getTagSha, getTag, getLightTag, getCommits, checkTagExists, createTag, f
 
 function setOutput(key, value) {
   const output = process.env['GITHUB_OUTPUT']
+  const invalidChars = "##"
+  if(value.includes(invalidChars)){
+    value = value.replace(invalidChars, "");
+  }
   // Temporary hack until core actions library catches up with github new recommendations
   // TODO: check for updates
     fs.appendFileSync(output, `${key}=${value}${os.EOL}`)
