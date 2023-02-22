e35117fbd2efb34003f49dc45d50f679b254f138
GitHub Action <action@github.com>
2023-02-22 17:10:19 +0000

    Pack with dependencies to dist/index.js

--- dist/index.js
+++ dist/index.js
@@ -26,7 +26,7 @@ function setOutput(key, value) {
   const invalidChars = "#"
   
   if(typeof(value) === 'string' && value.includes(invalidChars)){
-    value = value.replace(invalidChars, "");
+    value = value.replaceAll(invalidChars, "");
   }
   // Temporary hack until core actions library catches up with github new recommendations
   // TODO: check for updates
