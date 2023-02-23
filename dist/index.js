5aaf848c8e4ac3f602cc00711fa72d75dc019919
GitHub Action <action@github.com>
2023-02-23 17:38:53 +0000

    Pack with dependencies to dist/index.js

--- dist/index.js
+++ dist/index.js
@@ -24,13 +24,13 @@ const { getTagSha, getTag, getLightTag, getCommits, checkTagExists, createTag, f
 function setOutput(key, value) {
   const output = process.env['GITHUB_OUTPUT']
   const invalidChars = "#"
-  
-  if(typeof(value) === 'string' && value.includes(invalidChars)){
-    value = value.replaceAll(value, "");
-  }
-  // Temporary hack until core actions library catches up with github new recommendations
-  // TODO: check for updates
-    fs.appendFileSync(output, `${key}=${value}${os.EOL}`)
+  core.setOutput(key,value);
+  // if(typeof(value) === 'string' && value.includes(invalidChars)){
+  //   value = value.replaceAll(value, "");
+  // }
+  // // Temporary hack until core actions library catches up with github new recommendations
+  // // TODO: check for updates
+  //   fs.appendFileSync(output, `${key}=${value}${os.EOL}`)
 }
 async function run() {
   try {
