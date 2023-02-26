d06a1b569d63b5600f231d636f9f7bd10aa8929f
GitHub Action <action@github.com>
2023-02-26 12:54:13 +0000

    Pack with dependencies to dist/index.js

--- dist/index.js
+++ dist/index.js
@@ -104,12 +104,13 @@ async function run() {
       core.setFailed(`Nothing to bump - not building release`);
       return;
     }
-    const {newVersion, newTag} = await utils.calculateVersion(tag, branch, bump, preRelease, defaultBump)
+    const {newVersion, newTag, newNumbered} = await utils.calculateVersion(tag, branch, bump, preRelease, defaultBump)
     
     core.info(`New version: ${newVersion}, New Tag: ${newTag}`)
 
     core.setOutput("new_version", newVersion);
     core.setOutput("new_tag", newTag);
+    core.setOutput("new_numbered", newNumbered);
 
     core.debug(`New tag: ${newTag}`);
 
@@ -78290,7 +78291,8 @@ async function calculateVersion(tag, branch, bump, preRelease, defaultBump = "pa
   
   newTag = newTag.replace(/_/g, '-');
   newVersion = newVersion.replace(/_/g, '-');
-  return {newVersion, newTag}
+  newNumbered = newTag.indexOf("-") > 0 ? newTag.substring(0, newTag.indexOf("-")) : newTag;
+  return {newVersion, newTag, newNumbered}
 }
 
 module.exports = { calculateVersion }
